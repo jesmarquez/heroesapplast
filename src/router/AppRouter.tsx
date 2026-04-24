3072e12c2bed3c1c10830075f1e8b4d547a0f7af
Dell_JESMQZ <jesmqz@gmail.com>
2026-04-24 10:03:14 -0400

    Lazy load

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,8 +4,10 @@ import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { AdminPage } from "@/heroes/pages/admin/AdminPage";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
+import { lazy } from "react";
+// import { SearchPage } from "@/heroes/pages/search/SearchPage";
 
+const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'))
 export const appRouter = createBrowserRouter([
   {
     path: '/',
